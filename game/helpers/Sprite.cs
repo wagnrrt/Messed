526d275b574fe24b2ea374b5a3312f1dacbc3688
wagnrrt <wagnrb0@gmail.com>
2025-03-12 20:59:33 -0300

    Adjustments to the Sprite.cs class

--- game/helpers/Sprite.cs
+++ game/helpers/Sprite.cs
@@ -5,12 +5,22 @@ namespace capybara;
 
 public class Sprite
 {
-	public Texture2D tex;
-	public Vector2 pos;
+  private Texture2D tex;
+	private Vector2 pos;
+	private byte scale; 
 
-	public Sprite(Texture2D tex, Vector2 pos)
+	public Sprite(Texture2D tex, Vector2 pos, byte scale)
 	{
 		this.tex = tex;
 		this.pos = pos;
+		this.scale = scale;
+	}
+
+	public Rectangle Rect
+	{
+		get
+		{
+			return new Rectangle((int)pos.X, (int)pos.Y, scale, scale);
+		}
 	}
 }
